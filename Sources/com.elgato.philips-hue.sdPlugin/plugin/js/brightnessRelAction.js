5ffc1cd741e4053b7436f16e7eb6d72fd7450bf6
dkern <git@eisbehr.de>
2022-01-07 11:38:07 +0100

    add dynamic icons to brightness action and cleanup code

--- Sources/com.elgato.philips-hue.sdPlugin/plugin/js/brightnessRelAction.js
+++ Sources/com.elgato.philips-hue.sdPlugin/plugin/js/brightnessRelAction.js
@@ -1,16 +1,14 @@
-//==============================================================================
 /**
-@file       brightnessRelAction.js
-@brief      Philips Hue Plugin
-@copyright  (c) 2019, Corsair Memory, Inc.
-            This source code is licensed under the MIT-style license found in the LICENSE file.
-**/
-//==============================================================================
+@file      brightnessRelAction.js
+@brief     Philips Hue Plugin
+@copyright (c) 2019, Corsair Memory, Inc.
+@license   This source code is licensed under the MIT-style license found in the LICENSE file.
+*/
 
 // Prototype which represents a brightness action
 function BrightnessRelAction(inContext, inSettings) {
     // Init BrightnessRelAction
-    var instance = this;
+    let instance = this;
 
     // Inherit from Action
     Action.call(this, inContext, inSettings);
@@ -19,11 +17,14 @@ function BrightnessRelAction(inContext, inSettings) {
     setDefaults();
 
     // Public function called on key up event
-    this.onKeyUp = function(inContext, inSettings, inCoordinates, inUserDesiredState, inState) {
+    this.onKeyUp = (inContext, inSettings, inCoordinates, inUserDesiredState, inState) => {
         // If onKeyUp was triggered manually, load settings
         if (inSettings === undefined) {
             inSettings = instance.getSettings();
         }
+
+        // Set icon according to relative value
+        setState(inContext, inSettings.brightnessRel >= 0 ? 0 : 1);
 
         // Check if any bridge is configured
         if (!('bridge' in inSettings)) {
@@ -34,13 +35,13 @@ function BrightnessRelAction(inContext, inSettings) {
 
         // Check if the configured bridge is in the cache
         if (!(inSettings.bridge in cache.data)) {
-            log('Bridge ' + inSettings.bridge + ' not found in cache');
+            log(`Bridge ${inSettings.bridge} not found in cache`);
             showAlert(inContext);
             return;
         }
 
         // Find the configured bridge
-        var bridgeCache = cache.data[inSettings.bridge];
+        let bridgeCache = cache.data[inSettings.bridge];
 
         // Check if any light is configured
         if (!('light' in inSettings)) {
@@ -51,12 +52,12 @@ function BrightnessRelAction(inContext, inSettings) {
 
         // Check if the configured light or group is in the cache
         if (!(inSettings.light in bridgeCache.lights || inSettings.light in bridgeCache.groups)) {
-            log('Light or group ' + inSettings.light + ' not found in cache');
+            log(`Light or group ${inSettings.light} not found in cache`);
             showAlert(inContext);
             return;
         }
 
-        // Check if any color is configured
+        // Check if any brightness is configured
         if (!('brightnessRel' in inSettings)) {
             log('No relative brightness configured');
             showAlert(inContext);
@@ -64,10 +65,10 @@ function BrightnessRelAction(inContext, inSettings) {
         }
 
         // Create a bridge instance
-        var bridge = new Bridge(bridgeCache.ip, bridgeCache.id, bridgeCache.username);
+        let bridge = new Bridge(bridgeCache.ip, bridgeCache.id, bridgeCache.username);
 
         // Create a light or group object
-        var objCache, obj;
+        let objCache, obj;
         if (inSettings.light.indexOf('l') !== -1) {
             objCache = bridgeCache.lights[inSettings.light];
             obj = new Light(bridge, objCache.id);
@@ -77,25 +78,26 @@ function BrightnessRelAction(inContext, inSettings) {
             obj = new Group(bridge, objCache.id);
         }
 
-        // Convert brightness 
+        // Convert brightness
+        let brightness;
         if (objCache.power) {
-            var birghtnessRel = (objCache.brightness / 2.54) + parseInt(inSettings.brightnessRel);
-            var brightness = Math.round(birghtnessRel * 2.54);
+            let brightnessRel = (objCache.brightness / 2.54) + parseInt(inSettings.brightnessRel);
+            brightness = Math.round(brightnessRel * 2.54);
         }
         else {
-            var brightness = parseInt(inSettings.brightnessRel);
+            brightness = parseInt(inSettings.brightnessRel);
         }
-        
+
         if (brightness > 254) {
             brightness = 254;
         }
         else if (brightness < 0) {
             brightness = 0;
         }
-        
+
         // Turn lights off if brightness is 0
         if (brightness <= 0) {
-            obj.setPower(false, function(inSuccess, inError) {
+            obj.setPower(false, (inSuccess, inError) => {
                 if (inSuccess) {
                     objCache.power = false;
                 }
@@ -107,7 +109,7 @@ function BrightnessRelAction(inContext, inSettings) {
         }
         else {
             // Set light or group state
-            obj.setBrightness(brightness, function(inSuccess, inError) {
+            obj.setBrightness(brightness, (inSuccess, inError) => {
                 if (inSuccess) {
                     objCache.birghtness = brightness;
                 }
@@ -120,12 +122,12 @@ function BrightnessRelAction(inContext, inSettings) {
     };
 
     // Before overwriting parent method, save a copy of it
-    var actionNewCacheAvailable = this.newCacheAvailable;
+    let actionNewCacheAvailable = this.newCacheAvailable;
 
     // Public function called when new cache is available
-    this.newCacheAvailable = function(inCallback) {
+    this.newCacheAvailable = inCallback => {
         // Call actions newCacheAvailable method
-        actionNewCacheAvailable.call(instance, function() {
+        actionNewCacheAvailable.call(instance, () => {
             // Set defaults
             setDefaults();
 
@@ -137,8 +139,8 @@ function BrightnessRelAction(inContext, inSettings) {
     // Private function to set the defaults
     function setDefaults() {
         // Get the settings and the context
-        var settings = instance.getSettings();
-        var context = instance.getContext();
+        let settings = instance.getSettings();
+        let context = instance.getContext();
 
         // If brightness is already set for this action
         if ('brightnessRel' in settings) {
